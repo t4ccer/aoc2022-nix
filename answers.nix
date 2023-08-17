ae46d8aab7986d69dd3a89bda9176c5f715de2d5
t4ccer <t4ccer@gmail.com>
2023-08-17 05:02:37 +0200

    Prefix single-digit days with 0

--- answers.nix
+++ answers.nix
@@ -1,37 +1,37 @@
 {
-  day1 = {
+  day01 = {
     part1 = 71924;
     part2 = 210406;
   };
-  day2 = {
+  day02 = {
     part1 = 15691;
     part2 = 12989;
   };
-  day3 = {
+  day03 = {
     part1 = 8401;
     part2 = 2641;
   };
-  day4 = {
+  day04 = {
     part1 = 567;
     part2 = 907;
   };
-  day5 = {
+  day05 = {
     part1 = "JCMHLVGMG";
     part2 = "LVMRWSSPZ";
   };
-  day6 = {
+  day06 = {
     part1 = 1794;
     part2 = 2851;
   };
-  day7 = {
+  day07 = {
     part1 = 1367870;
     part2 = 549173;
   };
-  day8 = {
+  day08 = {
     part1 = 1703;
     part2 = 496650;
   };
-  day9 = {
+  day09 = {
     part1 = 6190;
     part2 = 2516;
   };
--- default.nix
+++ default.nix
@@ -26,15 +26,15 @@ let
   };
 in
   builtins.listToAttrs (map mkDay [
-    ./day1
-    ./day2
-    ./day3
-    ./day4
-    ./day5
-    ./day6
-    ./day7
-    ./day8
-    ./day9
+    ./day01
+    ./day02
+    ./day03
+    ./day04
+    ./day05
+    ./day06
+    ./day07
+    ./day08
+    ./day09
     ./day10
     ./day11
   ])
